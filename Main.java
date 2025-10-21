cb0f51eb6ae32fd09a88dae1b8bd95577cdf462d
gloomyzx <78573906+RubioJoan@users.noreply.github.com>
2025-10-21 04:33:05 +0000

    Create New Java File (Toy)

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello World");
         Plant plant1 =new Plant();
         plant1.name = "papaya";
+        plant1.branch = 3; //sample only
         System.out.println(plant1.name);
         System.out.println(plant1.getBranch());
     }
@@ -15,5 +16,7 @@ class Plant {
         this.leaves=leaves;
     }
     int getBranch(){
+        return branch;
+
     }
 }
---
+++ Toy.java
@@ -0,0 +1,26 @@
+public class Toy {
+    String name;
+    String brand;
+    double price;
+    int quantity;
+
+    void setPrice(double price2){
+        this.price =price2;
+    }
+    /* THIS is a
+     * multiline
+     * comment
+     */
+
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+        toy1.name ="rare pink";
+        toy1.brand ="Lab vuvu";
+        toy1.price =4500;
+        toy1.quantity =12;  
+        toy1.setPrice((toy1.price*0.5));
+        System.out.println(toy1.price);
+        System.out.println(toy1.name);
+    }
+
+}
