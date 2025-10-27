c9921933dd2fbbfa4eb00ceef0976f7a0fb1cea8
gloomyzx <78573906+RubioJoan@users.noreply.github.com>
2025-10-27 11:21:46 +0000

    Task 2- October 27, 2025

---
+++ Task2.java
@@ -0,0 +1,15 @@
+public class Task2 {
+public static void main(String[] args) {
+    byte zero = 0;
+    short one =1;
+    int three = 3;
+    float tpz = 2.0f;
+    char h = 'H';
+    char w = 'w';
+    boolean tru = true;
+
+
+    String output = h + "" + three + one + one + zero + " " + w + zero + "r"  + one + "d" + " "  + tpz + " " + tru;
+    System.out.println(output);
+}
+}
