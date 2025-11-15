8a4b475bc1c1dfe404d0eb6d3a9d47cf424076b7
gloomyzx <78573906+RubioJoan@users.noreply.github.com>
2025-11-15 00:28:33 +0000

    Created Task4

---
+++ Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+    public static void main(String[] args) {
+        StringBuilder pal = new StringBuilder("Animals");
+        pal.reverse();
+        System.out.println("The input string is palindrome");
+        System.out.println("The input string is not palindrome
+        ");
+
+    }
+}
