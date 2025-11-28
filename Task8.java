bd63a6261950cf30cae8f9ab3cb43fc2707b58a5
gloomyzx <78573906+RubioJoan@users.noreply.github.com>
2025-11-28 13:57:38 +0000

    Updated

--- Task8.java
+++ Task8.java
@@ -1,3 +1,23 @@
 public class Task8 {
+    public static int cumulativeArgs(int... numbers){
+        int total =0;
+
+        for(int n: numbers){
+            int cumulative = 0;
+
+            for (int i = 1; i <= n; i++){
+                cumulative +=i;
+            }
+
+            System.out.println("Cumulative sum for "+ n  + " = " + cumulative);
+            total += cumulative;
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        int result = cumulativeArgs(4,5,10);
+        System.out.println("The total sum of all cumulative values :" + result);
+        
+    }
     
 }
