40cb1053a66974908b7b504ce46a952237c8a0d7
gloomyzx <78573906+RubioJoan@users.noreply.github.com>
2025-11-28 13:33:11 +0000

    Updated Task7

---
+++ Task7.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Task7 {
+
+    static int add (int a, int b){
+        return a + b;
+    }
+
+    static int subtract (int a, int b){
+        return a - b;
+    }
+
+    static int multiply (int a, int b){
+        return a * b;
+    }
+    
+    static double divide (int a, int b){
+        return (double) a/b;
+    }
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: " );
+        a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        b = input.nextInt();
+
+        System.out.println("\nPERFORMING 4 BASIC ARITHMETIC OPERATIONS");
+        System.out.println("\nADDITION: " + add(a, b));
+        System.out.println("SUBTRACTION: " + subtract(a, b));
+        System.out.println("MULTIPLICATION: "+ multiply(a, b));
+
+        if (b != 0) {
+            System.out.println("DIVISION: " + divide(a, b));
+        } else {
+            System.out.println("Division: Cannot divide by zero!");
+        }
+
+    input.close();
+    
+    }
+    
+
+}
