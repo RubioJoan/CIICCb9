0480dd2e4829372176647cc99a8fce79d07ff345
gloomyzx <78573906+RubioJoan@users.noreply.github.com>
2025-11-28 14:12:54 +0000

    Updated

--- Task10.java
+++ Task10.java
@@ -1,22 +1,26 @@
 public class Task10{
-  class Student {
+  static class Student {
     private String firstName;
     private String lastName;
     public Student(String firstName, String lastName){
       this.firstName = firstName;
       this.lastName = lastName;
     }
-    
-  }
+    public void printFullName(){
+      System.out.println(firstName + " "+ lastName);
+    }
+  }   
   public class TaskSheet10{
     public static void main (String[] args){
-      Student [] studnets = new Student []{
+      Student [] students = new Student []{
         new Student ("Morgan", "Freemaan"),
         new Student ("Brad", "Pitt"),
-        new Student ("Kevin", Spaacey"),
+        new Student ("Kevin", "Spacey"),
       };
-      for (Students s: students){
+      for (Student s: students){
         s.printFullName();
       }
     }
   }
+}
+
